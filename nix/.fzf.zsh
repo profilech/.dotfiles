d96a8203daf162748fbe05cc1e9e0f00b27cd024
JuneQ Cheung <a1624jz@aiit.ac.jp>
2018-06-12 15:53:16 +0900

    Fix fzf sh

--- nix/.fzf.zsh
+++ nix/.fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/user01/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/user01/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/user01/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/user01/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
 
